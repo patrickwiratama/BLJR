bac38a85f6382dcef3f76eedde75c1f5fd557414
Acezz4120 <arthurandersennn@gmail.com>
2021-02-13 17:26:12 +0700

    arthur changes (database&registration)
    database ditambah
    registration form diubah dan selesai

--- app/src/main/java/com/example/bljr/DatabaseHelper.java
+++ app/src/main/java/com/example/bljr/DatabaseHelper.java
@@ -1,39 +1,58 @@
 package com.example.bljr;
+
 import android.content.Context;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper{
-    public static final String DATABASE_NAME = "registrationForm.db";
-    public static final String TABLE_NAME = "register";
-    public static final String COL_1 = "firstName";
-    public static final String COL_2 = "lastName ";
-    public static final String COL_3 = "email";
-    public static final String COL_4 = "Tempat/Tanggal Lahir";
-    public static final String COL_5 = "University";
-    public static final String COL_6 = "Year";
-    public static final String COL_7 = "Major";
-    public static final String COL_8 = "Gender";
-    public static final String COL_9 = "Password";
-    public static final String COL_10 = " ConfirmPassword";
+    public static final String DATABASE_NAME = "login.db";
 
-    public DatabaseHelper(@Nullable Context context) {
-        super(context, DATABASE_NAME, null, 1);
+    public DatabaseHelper(Context context){
+        super(context,DATABASE_NAME, null, 1);
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE" + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTO INCREMENT, firstName TEXT, lastName, TEXT, email TEXT, Tempat/Tanggal lahir TEXT, University TEXT, Year TEXT, Major TEXT, Gender TEXT, Password TEXT, ConfirmPassword TEXT)");
-
+    public void onCreate (SQLiteDatabase db){
+        db.execSQL("Create Table user (ID Integer Primary Key AutoIncrement, userName Text, password Text)");
     }
 
-    @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("FROM TABLE IF EXISTS" + TABLE_NAME); //Drop older table if exists
-        onCreate(db);
+        db.execSQL("Drop Table if Exists user");
     }
 
-    public
+    public boolean Insert (String userName, String password){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("firstName", userName);
+        contentValues.put("lastName", password);
+        long result = sqLiteDatabase.insert("user", null, contentValues);
+        if (result == -1){
+            return false;}
+            else{
+                return true;
+            }
+        }
+        public Boolean CheckUserName(String userName, String password){
+            SQLiteDatabase sqLiteDatabase1 = this.getWritableDatabase();
+            Cursor cursor = sqLiteDatabase1.rawQuery("SELECT * FROM user Where Username=?", new String[]{userName,password});
+            if (cursor.getCount()>0){
+                return false;}
+                else{
+                    return true;
+                }
+            }
+
+    public Boolean Checkpassword(String userName, String password){
+        SQLiteDatabase sqLiteDatabase1 = this.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase1.rawQuery("SELECT * FROM user Where Username=? AND password=?", new String[]{userName,password});
+        if (cursor.getCount()>0){
+            return true;}
+        else{
+            return false;
+        }
+    }
 }
--- app/src/main/java/com/example/bljr/registrationForm.java
+++ app/src/main/java/com/example/bljr/registrationForm.java
@@ -1,71 +1,75 @@
 package com.example.bljr;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 public class registrationForm extends AppCompatActivity {
+    DatabaseHelper databaseHelper;
+
+    EditText et_firstName, et_lastName, et_password, et_confirmPassword, et_email, et_University, et_Year, et_Major, et_Gender, et_DoB;
+    Button btn_register, btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration_form);
-        SQLiteOpenHelper openHelper;
-        SQLiteDatabase db;
-        Button _btnreg;
-        EditText _txtfirstName, _txtlastName, _txtlemail, _txtTempatTanggalLahir, _txtUniversity, _txtYear, _txtMajor, _txtGender, _txtPassword, _txtConfirmPassword;
-        Object String;
-        openHelper=new DatabaseHelper(this);
-        setContentView(R.layout.activity_main);
-        _btnreg = (Button)findViewById(R.id.btnreg);
-        _txtfirstName=(EditText)findViewById(R.id.firstName);
-        _txtlastName=(EditText)findViewById(R.id.textlastName);
-        _txtlemail=(EditText)findViewById(R.id.textlemail);
-        _txtTempatTanggalLahir=(EditText)findViewById(R.id.textTempatTanggalLahir);
-        _txtUniversity=(EditText)findViewById(R.id.textUniversity);
-        _txtYear=(EditText)findViewById(R.id.textYear);
-        _txtMajor=(EditText)findViewById(R.id.textMajor);
-        _txtGender=(EditText)findViewById(R.id.textGender);
-        _txtPassword=(EditText)findViewById(R.id.textPassword);
-        _txtConfirmPassword= findViewById(R.id.textConfirmPassword);
-        _btnreg.setOnClickListener(new View.onClockListener() {
-            public void onClock(View v) {
-                db = openHelper.getWritableDatabase();
-                //String fname = _txtfanme.getText().toString();
-                String firstName = _txtfirstName.getText().toString();
-                String lastName = _txtlastName.getText().toString();
-                String lemail = _txtlemail.getText().toString();
-                String TempatTanggalLahir = _txtTempatTanggalLahir.getText().toString();
-                String University = _txtUniversity.getText().toString();
-                String Year = _txtYear.getText().toString();
-                String Major = _txtMajor.getText().toString();
-                String Gender = _txtGender.getText().toString();
-                String Password = _txtPassword.getText().toString();
-                String ConfirmPassword = _txtConfirmPassword.getText().toString();
-                insertData(firstName, lastName, lemail, TempatTanggalLahir, University, Year, Major, Gender, Password, ConfirmPassword);
-                Toast.makeText(getApplicationContext(), "Register Successfully", Toast.LENGTH_LONG.show());
-            }
+       setContentView(R.layout.activity_main);
 
-            public void insertData (String firstName, String lastName, String lemail, String TempatTanggalLahir, String University, String Year, String Major, String Gender, String Password, String ConfirmPassword ) {
-                ContentValues contentValues = new ContentValues();
-                //ContentValues.put(DatabaseHelper.COL_2,fname);
-                ContentValues.put(DatabaseHelper.COL_2,firstName);
-                ContentValues.put(DatabaseHelper.COL_3,lastName);
-                ContentValues.put(DatabaseHelper.COL_4,TempatTanggalLahir);
-                ContentValues.put(DatabaseHelper.COL_5,University);
-                ContentValues.put(DatabaseHelper.COL_6, Year);
-                ContentValues.put(DatabaseHelper.COL_7,Major);
-                ContentValues.put(DatabaseHelper.COL_8,Gender);
-                ContentValues.put(DatabaseHelper.COL_9,Password);
-                ContentValues.put(DatabaseHelper.COL_10,ConfirmPassword);
-                long id = db.insert(DatabaseHelper.TABLE_NAME, null, contentValues);
-            }
+        databaseHelper = new DatabaseHelper(this);
+        et_firstName = (EditText) findViewById(R.id.et_firstName);
+        et_lastName = (EditText) findViewById(R.id.et_lastName);
+        et_password = (EditText) findViewById(R.id.et_password);
+        et_confirmPassword = (EditText) findViewById(R.id.et_confirmPassword);
+        et_University = (EditText) findViewById(R.id.et_University);
+        et_Major = (EditText) findViewById(R.id.et_Major);
+        et_Year = (EditText) findViewById(R.id.et_Year);
+        et_email = (EditText) findViewById(R.id.et_email);
+        et_DoB = (EditText) findViewById(R.id.et_DoB);
+        btn_register = (Button) findViewById(R.id.btn_register);
+        //btn_login = (Button) findViewById(R.id.btn_login);
 
+        //btn_login.setOnClickListener(new View.OnClickListener() {
+           // @Override
+            //public void onClick(View v) {
+              //  Intent intent = new Intent(MainActivity.this, Login.class);
+               // startActivity(intent);
+            //}
+       // }
+        //);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = et_firstName.getText().toString();
+                String password = et_password.getText().toString();
+                String confirm_password = et_confirmPassword.getText().toString();
+
+                if (username.equals("") || password.equals("") || confirm_password.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Fields Required", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (password.equals(confirm_password)) {
+                        Boolean checkUserName = databaseHelper.CheckUserName(username, password);
+                        if (checkUserName == true) {
+                            Boolean insert = databaseHelper.Insert(username, password);
+                            if (insert == true) {
+                                Toast.makeText(getApplicationContext(), "Registered", Toast.LENGTH_SHORT).show();
+                                et_firstName.setText("");
+                                et_password.setText("");
+                                et_confirmPassword.setText("");
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Username already taken", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Password does not match", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
 }
