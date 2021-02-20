621bdf0d5a5e239a60af15a3c2e05af70dbd8375
Acezz4120 <arthurandersennn@gmail.com>
2021-02-20 18:05:49 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/bljr/MainActivity.java
+++ app/src/main/java/com/example/bljr/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bljr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
@@ -14,9 +15,23 @@ import android.widget.Toast;
 import java.time.Year;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button imageButton10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageButton10 = (Button) findViewById(R.id.button);
+        imageButton10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, registrationForm.class);
+        startActivity(intent);
     }
 }
